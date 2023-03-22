22ddabe1ee72b961c621c9996ccc6079cce9a528
Albérico Eduardo <albericoeduardo@users.noreply.github.com>
2023-03-22 17:53:18 -0300

    ATT code

--- src/containers/Work/Work.jsx
+++ src/containers/Work/Work.jsx
@@ -25,7 +25,18 @@ const Work = () => {
     
 
     const handleWorkFilter = (item) => {
+      setActiveFilter(item);
+      setAnimateCard([{y:100, opacity: 0}]);
 
+      setTimeout(() => {
+        setAnimateCard([{y:100, opacity: 1}]);
+
+        if(item === 'All') {
+          setFilterWork(works);
+        }else{
+          setFilterWork(works.filter((work) => work.tags.includes(item)))
+        }
+      }, 500);
     }
 
   return (
@@ -59,7 +70,7 @@ const Work = () => {
                 transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
                 className='app__work-hover app__flex'
               >
-                <a href={work.projectLink} target='_blank' rel='noreferr'>
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{scale: [0,1]}}
                     whileHover={{scale: [1, 0.9]}}
@@ -69,7 +80,7 @@ const Work = () => {
                     <AiFillEye/>
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target='_blank' rel='noreferr'>
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{scale: [0,1]}}
                     whileHover={{scale: [1, 0.9]}}
